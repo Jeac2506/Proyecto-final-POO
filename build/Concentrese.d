844570463c4d9aa4c914c2a39c4fb06ffd6741b1
Famahu1785 <hurtado.jhoan@correounivalle.edu.co>
2025-06-17 01:00:47 +0000

    actual

--- build/Concentrese.d
+++
@@ -1,6 +0,0 @@
-build/Concentrese.o: src/Concentrese.cpp include/Concentrese.h \
- include/Juego.h
-
-include/Concentrese.h:
-
-include/Juego.h:
--- build/main.d
+++
@@ -1,8 +0,0 @@
-build/main.o: main.cpp include/Ahorcado.h include/Juego.h \
- include/Concentrese.h
-
-include/Ahorcado.h:
-
-include/Juego.h:
-
-include/Concentrese.h:
